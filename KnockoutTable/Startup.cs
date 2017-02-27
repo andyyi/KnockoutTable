f62d55151876f8d7f0af8ed1723569cd4f1d107b
andyyi <ayi@steadyrain.com>
2017-02-27 08:12:55 +0800

    Initialize project.

---
+++ KnockoutTable/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(KnockoutTable.Startup))]
+namespace KnockoutTable
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
